a6dbed37197c0f21ceb6c81baad482f550a1a671
Conor Barnes <conorbarnes93@gmail.com>
2020-09-08 12:09:22 -0700

    Remove old test files

--- capitalize.test.js
+++
@@ -1,9 +0,0 @@
-const capitalize = require('./capitalize')
-
-test('converts "yo dawg" to "Yo dawg"', () => {
-    expect(capitalize('yo dawg')).toBe("Yo dawg");
-});
-
-test('Converts "SUP" to "SUP"', () => {
-    expect(capitalize("SUP")).toBe("SUP");
-})
